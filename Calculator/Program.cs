ef1d2278ee53c5761a816ba6baf561eab28b05e2
Carlos Daniel <carlos.oficial.ti@gmail.com>
2024-05-27 16:32:22 -0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Calculator {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Primerio valor");
+        }
+    }
+}
