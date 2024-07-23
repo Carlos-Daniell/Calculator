b5e9828980ebac9668e1cb893c530393d6de5985
Carlos Daniel <carlos.oficial.ti@gmail.com>
2024-07-22 22:35:39 -0300

    Transformando as operações em funções/métodos separados

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,9 +4,44 @@ namespace Calculator {
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Digite Primerio valor");
-            float v1 = float.Parse(Console.ReadLine());
-            Console.WriteLine(v1);
+            Soma();
+
         }
+        static void Soma() {
+
+            Console.Write("Primerio valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float resultado = v1 + v2;
+            Console.Write($"O resultado da Soma é: {resultado}");
+
+        }
+        static void Subtracao() {
+            Console.Write("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é: {resultado}");
+        }
+        static void Divisao() {
+            Console.Write("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisão é: {resultado}");
+        }
+
     }
 }
