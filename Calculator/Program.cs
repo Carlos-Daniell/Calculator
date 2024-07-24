c6c153b97e6c00c0053f81ca5fbd967b3c563b9e
Carlos Daniel <carlos.oficial.ti@gmail.com>
2024-07-24 09:57:23 -0300

    Criação do Menu da  aplicação

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -3,8 +3,20 @@
 namespace Calculator {
     class Program {
         static void Main(string[] args) {
+            Menu();
 
-            Soma();
+        }
+        static void Menu() {
+            Console.WriteLine("Oque deseja fazer ?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
+
+            Console.WriteLine("-----------------");
+            Console.Write("Selecione uma opção: ");
+            short res = short.Parse(Console.ReadLine());
 
         }
         static void Soma() {
@@ -41,6 +53,17 @@ namespace Calculator {
             Console.WriteLine("");
             float resultado = v1 / v2;
             Console.WriteLine($"O resultado da divisão é: {resultado}");
+        }
+        public static void Mutiplicacao() {
+            Console.Write("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float resultado = v1 * v2;
+            Console.WriteLine($"O resultado da multiplicação é: {resultado}");
         }
 
     }
